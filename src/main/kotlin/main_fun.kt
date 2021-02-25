b8b0491da206fb09a442afb9c73ec36bc45929f2
ant1krinz <72338323+ant1krinz@users.noreply.github.com>
2021-02-25 22:22:30 +0300

    обновил rectangle, добавил создание экземпляра класса

--- src/main/kotlin/main_fun.kt
+++ src/main/kotlin/main_fun.kt
@@ -3,9 +3,12 @@ fun main(args: Array<String>) {
     var i:Int = 0
 
     var our_vertices: Array<point> = arrayOf(point(0.0, 0.0), point(0.0, 1.0), point(1.0,0.0))
+    var our_vertices_for_rectangle: Array<point> = arrayOf(point(0.0, 0.0), point(6.0, 7.0))
     var our_triangle:Triangle = Triangle(our_vertices)
     var our_square: Square = Square(our_vertices)
+    var our_rectangle: Rectangle = Rectangle(our_vertices_for_rectangle)
 
     println("Our triangle's area is ${our_triangle.Area()}")
     println("Our square's area is ${our_square.Area()}")
+    println("Our triangle's area is ${our_rectangle.Area()}")
 }
