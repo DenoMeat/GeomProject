f943f558b24ec5a32986a8d6821e50d069b6cac2
78KuBa78 <DimaKub05@gmail.com>
2021-02-25 20:09:16 +0300

    Square

--- src/main/kotlin/main_fun.kt
+++ src/main/kotlin/main_fun.kt
@@ -4,6 +4,8 @@ fun main(args: Array<String>) {
 
     var our_vertices: Array<point> = arrayOf(point(0.0, 0.0), point(0.0, 1.0), point(1.0,0.0))
     var our_triangle:Triangle = Triangle(our_vertices)
+    var our_square: Square = Square(our_vertices)
 
     println("Our triangle's area is ${our_triangle.Area()}")
+    println("Our square's area is ${our_square.Area()}")
 }
